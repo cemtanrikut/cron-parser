0b87da1abbc98f80709f2c9433b14de411ca4202
Cem Tanrikut <cemtanrikut@gmail.com>
2024-09-03 14:12:50 +0200

    complete main file

--- cmd/cron_parser/main.go
+++ cmd/cron_parser/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/cemtanrikut/cron-parser/cmd/cron_parser/main.go/pkg/cron"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./cron_parser \"<cron_expression>\"")
+		return
+	}
+
+	cronExpression := os.Args[1]
+
+	parsedFields, err := cron.ParseCronExpression(cronExpression)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	cron.FormatOutput(parsedFields)
 }
